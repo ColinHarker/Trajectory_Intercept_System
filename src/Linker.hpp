c6c8aa6ce5140394ffa9bbfdfc4447bb089ab278
Colin Harker <colinharker5@yahoo.com>
2020-05-13 11:12:34 -0400

    Create Linker.hpp

---
+++ src/Linker.hpp
@@ -0,0 +1,15 @@
+#ifndef Linker_H
+#define Linker_H
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/opencv.hpp"
+
+#include "Library/SerialPort.hpp"
+#include "constants.hpp"
+#include "Port.hpp"
+#include "TrajectoryCalc.hpp"
+
+
+#endif // !Linker_H
